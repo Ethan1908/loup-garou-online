129b041a551c9774fef9169a0062d86e41a16af1
Ethan1908 <ethanprof59@outlook.fr>
2025-05-29 13:18:45 +0200

    Corrige la config Vite et met à jour le server/package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // ou '0.0.0.0' si tu accèdes depuis un autre appareil
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173,
+    },
+  },
 })
